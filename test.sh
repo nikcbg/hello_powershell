1de308e902dadf02db743769f58580c25d8c49c2
nikcbg2 <nick.hogan181@gmail.com>
2018-11-09 13:21:46 +0200

    adding test.sh file

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env powershell
+
+out=$(pwsh ./hello.ps1)
+
+# we should check out == hello
+if [ "${out}" == "hello" ];then
+  echo "GOOD: test pass"
+else
+  echo "BAD: test fail"
+  exit 1
+
